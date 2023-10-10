c5439ab746ce9310f50e28826a45b9f9629a4c4b
Allen Walter F. De Guzman <allenwalterfiestada@gmail.com>
2023-10-10 12:12:03 +0800

    first commit

---
+++ src/components/about/leaders/Data.js
@@ -0,0 +1,26 @@
+
+let leaders = [
+	{
+		name: 'Matt Marcial',
+		desc: 'passionate about cybersecurity and read teaming stuff',
+		skills: 'Team Leader | ',
+		nickname: "blueMatter",
+		avatar: require('../../../images/avatars/2.jpg')
+	},
+	{
+		name: 'Allen De Guzman',
+		desc: 'full stack javascript developer, cyber security lover and problem solver',
+		skills: 'Full Sack Developer | ',
+		nickname: "AllenDG",
+		avatar: require('../../../images/avatars/1.jpg')
+	},
+	{
+		name: 'Jedyne Claveria',
+		desc: 'creative with a way you cannot even imagine',
+		skills: 'Researcher |',
+		nickname: "Prim",
+		avatar: require('../../../images/avatars/3.jpg')
+	}
+]
+
+export {leaders}
---
+++ src/components/landing/testimonials/Data.js
@@ -0,0 +1,30 @@
+
+let tes = [
+	{
+		img: require('../../../images/tes/1.jpg'),
+		name: 'Allen Walter F. De Guzman',
+		skill: 'Full Stack Dev/Researcher'
+	},
+	{
+		img: require('../../../images/tes/2.jpg'),
+		name: 'Jedyne Ziah B. Claveria',
+		skill: 'Researcher'
+	},
+	{
+		img: require('../../../images/tes/3.jpg'),
+		name: 'Matt Adrian D. Marcial',
+		skill: 'Team Leader'
+	},
+	{
+		img: require('../../../images/tes/3.jpg'),
+		name: 'Jhulyan Estrada',
+		skill: 'Backup Researcher'
+	},
+	{
+		img: require('../../../images/tes/3.jpg'),
+		name: 'Jeremy Ferrer',
+		skill: 'Backup Researcher'
+	}
+]
+
+export {tes}
